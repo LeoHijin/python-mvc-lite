681ed0fa8ee61f954fb320c6e53b4ffd60a680f9
Leonardo <leonard_156@hotmail.com>
2014-10-02 14:37:57 -0500

    MODIFICADO PARA SU DISTRIBUCION

--- application/settings.py
+++ application/settings.py
@@ -4,13 +4,13 @@
 #BASES DE DATOS
 
 # Ruta del directorio que contiene toda la aplicacion
-APP_DIR = "/var/www/html/pylite.local/"
+APP_DIR = "/ruta/hacia/app_dir/"
 
 #Vista por defecto de la aplicacion
 DEFAULT_VIEW = "/users/user/index"
 
 # Ruta del directorio que contiene los archivos html, imagenes, js , iconos, etc
-WEB_ROOT = "/var/www/html/pylite.local/public_html/"
+WEB_ROOT = "/ruta/hacia/app_dir/public_html/"
 
 # Ruta de la vista por defecto (despues del dominio)
 WEB_DIR = "/"
